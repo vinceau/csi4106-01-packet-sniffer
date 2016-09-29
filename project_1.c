ec421613f7ef46ccc254fa2f3ff4c47f020c5e72
Vince Au <vinceau09@gmail.com>
2016-09-30 01:20:29 +0900

    Working packet capture

--- project_1.c
+++ project_1.c
@@ -1,22 +1,76 @@
 #include <stdio.h>
 #include <pcap.h>
 
+/*
+ * What should we do with the packet?
+ */
+void
+got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
+{
+	printf("received packet!\n");
+	return;
+}
 
-int main(int argc, char *argv[]) {
-    char *dev;    
-    char errbuf[PCAP_ERRBUF_SIZE];
+int
+main(int argc, char **argv)
+{
+	char *dev = NULL;                    /* capture device name */
+	char errbuf[PCAP_ERRBUF_SIZE];       /* error buffer */
+	pcap_t *handle;                      /* packet capture handle */
 
-    // get network device
-    dev = pcap_lookupdev(errbuf);
+	char filter_exp[] = "tcp port 80";   /* filter expression [3] */
+	struct bpf_program fp;               /* compiled filter program (expression) */
+	bpf_u_int32 mask;                    /* subnet mask */
+	bpf_u_int32 net;                     /* ip */
+	int num_packets = -1;                /* number of packets to capture */
 
-    if(dev == NULL)
-    {
-        printf("%s\n",errbuf);
-        return 0;
-    }
+	/* get the default device */
+	dev = pcap_lookupdev(errbuf);
+	if (dev == NULL) {
+		fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
+		return(2);
+	}
 
-    // print device name
-    printf("DEV: %s\n",dev);
+	/* get network number and mask associated with capture device */
+	if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) {
+		fprintf(stderr, "Couldn't get netmask for device %s: %s\n", dev, errbuf);
+		net = 0;
+		mask = 0;
+	}
 
+	/* print capture info */
+	printf("Sniffing on device: %s\n", dev);
+
+	/* open capture device */
+	handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+	if (handle == NULL) {
+		fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
+		return(2);
+	}
+
+	/* compile the filter expression */
+	if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
+		fprintf(stderr, "Couldn't parse filter %s: %s\n",
+			filter_exp, pcap_geterr(handle));
+		return(2);
+	}
+
+	/* apply the compiled filter */
+	if (pcap_setfilter(handle, &fp) == -1) {
+		fprintf(stderr, "Couldn't install filter %s: %s\n",
+			filter_exp, pcap_geterr(handle));
+		return(2);
+	}
+
+	/* now we can set our callback function */
+	pcap_loop(handle, num_packets, got_packet, NULL);
+
+	/* cleanup */
+	pcap_freecode(&fp);
+	pcap_close(handle);
+
+	printf("\nCapture complete.\n");
+
+	return 0;
 }
 
