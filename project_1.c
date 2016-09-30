85d7bdb49fdb7b18b7d307ac351f59e41d6fff75
Vince Au <vinceau09@gmail.com>
2016-09-30 19:06:07 +0900

    Add ethernet structures and type casting

--- project_1.c
+++ project_1.c
@@ -1,5 +1,69 @@
+/* Some ethernet structure declarations were taken from examples owned by the Tcpdump
+ * group and can be found at: http://www.tcpdump.org/pcap.html
+ */
+
 #include <stdio.h>
 #include <pcap.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+/* ethernet headers are always exactly 14 bytes [1] */
+#define SIZE_ETHERNET 14
+
+/* Ethernet addresses are 6 bytes */
+#define ETHER_ADDR_LEN	6
+
+/* Ethernet header */
+struct sniff_ethernet {
+	u_char  ether_dhost[ETHER_ADDR_LEN];    /* destination host address */
+	u_char  ether_shost[ETHER_ADDR_LEN];    /* source host address */
+	u_short ether_type;                     /* IP? ARP? RARP? etc */
+};
+
+/* IP header */
+struct sniff_ip {
+	u_char  ip_vhl;                 /* version << 4 | header length >> 2 */
+	u_char  ip_tos;                 /* type of service */
+	u_short ip_len;                 /* total length */
+	u_short ip_id;                  /* identification */
+	u_short ip_off;                 /* fragment offset field */
+	#define IP_RF 0x8000            /* reserved fragment flag */
+	#define IP_DF 0x4000            /* dont fragment flag */
+	#define IP_MF 0x2000            /* more fragments flag */
+	#define IP_OFFMASK 0x1fff       /* mask for fragmenting bits */
+	u_char  ip_ttl;                 /* time to live */
+	u_char  ip_p;                   /* protocol */
+	u_short ip_sum;                 /* checksum */
+	struct  in_addr ip_src,ip_dst;  /* source and dest address */
+};
+#define IP_HL(ip)               (((ip)->ip_vhl) & 0x0f)
+#define IP_V(ip)                (((ip)->ip_vhl) >> 4)
+
+/* TCP header */
+typedef u_int tcp_seq;
+
+struct sniff_tcp {
+	u_short th_sport;               /* source port */
+	u_short th_dport;               /* destination port */
+	tcp_seq th_seq;                 /* sequence number */
+	tcp_seq th_ack;                 /* acknowledgement number */
+	u_char  th_offx2;               /* data offset, rsvd */
+#define TH_OFF(th)      (((th)->th_offx2 & 0xf0) >> 4)
+	u_char  th_flags;
+	#define TH_FIN  0x01
+	#define TH_SYN  0x02
+	#define TH_RST  0x04
+	#define TH_PUSH 0x08
+	#define TH_ACK  0x10
+	#define TH_URG  0x20
+	#define TH_ECE  0x40
+	#define TH_CWR  0x80
+	#define TH_FLAGS        (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
+	u_short th_win;                 /* window */
+	u_short th_sum;                 /* checksum */
+	u_short th_urp;                 /* urgent pointer */
+};
+
 
 /*
  * What should we do with the packet?
@@ -7,7 +71,45 @@
 void
 got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
 {
-	printf("received packet!\n");
+	static int count = 1;                   /* packet counter */
+	printf("%d ", count++);
+	
+	const struct sniff_ethernet *ethernet; /* The ethernet header */
+	const struct sniff_ip *ip; /* The IP header */
+	const struct sniff_tcp *tcp; /* The TCP header */
+	const char *payload; /* Packet payload */
+
+	u_int size_ip;
+	u_int size_tcp;
+
+
+	ethernet = (struct sniff_ethernet*)(packet);
+	ip = (struct sniff_ip*)(packet + SIZE_ETHERNET);
+	size_ip = IP_HL(ip)*4;
+	if (size_ip < 20) {
+		printf("   * Invalid IP header length: %u bytes\n", size_ip);
+		return;
+	}
+	tcp = (struct sniff_tcp*)(packet + SIZE_ETHERNET + size_ip);
+	size_tcp = TH_OFF(tcp)*4;
+	if (size_tcp < 20) {
+		printf("   * Invalid TCP header length: %u bytes\n", size_tcp);
+		return;
+	}
+	payload = (u_char *)(packet + SIZE_ETHERNET + size_ip + size_tcp);
+
+	//printf("caplen: %u, len: %u\n", header->caplen, header->len);
+	//printf("packet contents:\n%s\n", packet);
+	
+	/* print source IP */
+	printf("%s:", inet_ntoa(ip->ip_src));
+	// source port
+	printf("%d ", ntohs(tcp->th_sport));
+
+	//destination ip
+	printf("%s:", inet_ntoa(ip->ip_dst));
+	//destination port
+	printf("%d HTTP\n", ntohs(tcp->th_dport));
 	return;
 }
 
@@ -19,7 +121,7 @@ main(int argc, char **argv)
 	pcap_t *handle;                      /* packet capture handle */
 
 	char filter_exp[] = "tcp port 80";   /* filter expression [3] */
-	struct bpf_program fp;               /* compiled filter program (expression) */
+	struct bpf_program fp;               /* compiled filter program */
 	bpf_u_int32 mask;                    /* subnet mask */
 	bpf_u_int32 net;                     /* ip */
 	int num_packets = -1;                /* number of packets to capture */
@@ -33,7 +135,8 @@ main(int argc, char **argv)
 
 	/* get network number and mask associated with capture device */
 	if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) {
-		fprintf(stderr, "Couldn't get netmask for device %s: %s\n", dev, errbuf);
+		fprintf(stderr, "Couldn't get netmask for device %s: %s\n", dev,
+			errbuf);
 		net = 0;
 		mask = 0;
 	}
